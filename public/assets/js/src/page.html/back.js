b49f8aa6e2bd938c6e780fef0654015a06c7aa96
IchBinHanz <nilssimons707@gmail.com>
2022-10-03 15:29:58 +0200

    2.3

--- public/assets/js/src/page.html/back.js
+++ public/assets/js/src/page.html/back.js
@@ -1,5 +1,5 @@
 const searchParams = new URLSearchParams(window.location.search);
 
 if (searchParams.has('from_query')) {
-    document.getElementById('back-url').href = 'http://localhost/?query='+searchParams.get('from_query')
+    document.getElementById('back-url').href = './?query='+searchParams.get('from_query')
 }
