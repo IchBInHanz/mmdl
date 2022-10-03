a4bb47be841a3bcb71218b2551afacb2f9a31c81
IchBinHanz <nilssimons707@gmail.com>
2022-10-04 01:21:12 +0200

    3.0

--- app/downloadTorrent.js
+++ app/downloadTorrent.js
@@ -8,20 +8,18 @@ async function downloadTorrent(socket, data) {
         DL_PATH = SERIES_DL_PATH
     }
     console.log('Started download...')
-    // exec(`webtorrent download "${data.magnet}" -o "${DL_PATH}"`, (error, stdout, stderr) => {
-    //     if (error) {
-    //         socket.emit('downloadTorrentErorr', error.message)
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         socket.emit('downloadTorrentErorr', stderr)
-    //         return;
-    //     }
-    //     console.log('Download finished...')
-    //     socket.emit('downloadTorrentSuccess')
-    // })
-    console.log('Download finished...')
-    socket.emit('downloadTorrentSuccess')
+    exec(`webtorrent download "${data.magnet}" -o "${DL_PATH}"`, (error, stdout, stderr) => {
+        if (error) {
+            socket.emit('downloadTorrentErorr', error.message)
+            return;
+        }
+        if (stderr) {
+            socket.emit('downloadTorrentErorr', stderr)
+            return;
+        }
+        console.log('Download finished...')
+        socket.emit('downloadTorrentSuccess')
+    })
 }
 
 exports.downloadTorrent = downloadTorrent
