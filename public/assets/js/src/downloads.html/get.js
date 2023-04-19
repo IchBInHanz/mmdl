2610fb51b9014e6a85c9fa5a58f7e77183253fdf
IchBinHanz <nilssimons707@gmail.com>
2023-04-19 10:55:45 +0200

    1.*

--- public/assets/js/src/downloads.html/get.js
+++ public/assets/js/src/downloads.html/get.js
@@ -7,7 +7,7 @@ socket.on('downloadsResults', (results) => {
         console.log(key, value)
         document.getElementById('downloads').innerHTML += `
         <div class="down-${value.status}">
-            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg">
+            <img src="${value.media_image}">
             <h5>${value.title}</h5>
         </div>
         `
